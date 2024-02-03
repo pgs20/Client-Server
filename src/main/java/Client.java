5401d2da420906adbdc4e3203fe16c14c6365446
Gosha <gosha_petrov20@rambler.ru>
2024-02-03 18:45:49 +0300

    Создал имитацию взаимодействия сервера и клиента

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8890;
+
+        try(Socket clientSocket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            out.println("Georgiy");
+            String res = in.readLine();
+            System.out.println(res);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
